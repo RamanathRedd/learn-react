5fbf83c4ef6ca084d2be3b8821a48c010848c820
ramanath.g <ramanathreddyg04@gmail.com>
2025-06-18 00:56:04 +0530

    deintiialize

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let state:{count:number,name:string}={
+    count:1,
+    name:"ram"
+  }
+  let {count,name}=state;
   return (
     <div className="App">
-      <h1>Welcome</h1>
+      <h1>{count}</h1>
+      <h1>{name}</h1>
     </div>
   );
+
 }
 
 export default App;
